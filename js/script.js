a40a300b0aa6507fc49aef0dae3298bfb7bb5f9d
gioanbatan <giovannibassani@hotmail.com>
2022-10-21 17:34:26 +0200

    Dinamic String images

--- js/script.js
+++ js/script.js
@@ -20,3 +20,56 @@
 // Buon lavoro e buon divertimento! :muscolo: :paperella:
 
 console.log(images);
+
+// MILESTONE 1
+// Prelelvare gli elementi in cui inserire i dati dell'array di oggetti
+//      main-pic per l'immagine
+//      text-pic per titolo e testo
+// PER ogni oggetto creare un elemento img in main-pic
+//       dal secondo elemento in poi hanno classe "hidden"
+// PER ogni oggetto creare un elemento h2 e p con titolo e testo in text-pic
+//      dal secondo elemento in poi hanno classe "hidden"
+//
+// Creare Event listner sulle frecce
+// Quando l'utente clicca una freccia
+//      viene aggiunta la classe "hidden" al testo dell'immagine corrente
+//      viene aggiunta la classe "hidden" all'immagine corrente
+//      immagine attuale aumenta di uno o diminuisce di uno
+//      viene tolta la classe "hidden" all'immagine corrente
+//      viene tolta la classe "hidden" al testo dell'immagine corrente
+
+// Output Elements
+let mainPicContainer = document.querySelector(".main-pic");
+console.log(mainPicContainer);
+const mainTextContainer = document.querySelector(".text-pic");
+// console.log(mainTextContainer);
+
+const prova = createImgElement(images);  
+// mainPicContainer.innerHTML = "test";
+// mainPicContainer.innerHTML = test;
+
+// console.log("CHIAMATA", test, typeof(test));
+
+
+// FUNCTIONS
+// Funzione che preleva il nome file di una foto e crea un <img>
+function createImgElement(objectsArray) {
+
+    let imagesElements = "";
+
+    for (let i = 0; i < objectsArray.length; i++) {
+        thisIndex = objectsArray[i];
+                
+        if (i === 0) {
+            imagesElements += `<img src="${thisIndex["image"]}" alt="${thisIndex["title"]}>`;
+        } else {
+            imagesElements += `<img class="hidden" src="${thisIndex["image"]}" alt="${thisIndex["title"]}>`;
+        }
+
+    }
+
+    console.log("SONO NELLA FUNZIONE", imagesElements);
+    mainPicContainer.innerHTML = imagesElements;
+
+    // return imagesElements;
+}
